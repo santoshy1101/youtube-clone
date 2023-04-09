7318b58fb07e473cc7eddcd36df23ab2e77e2ac8
santosh <styadav018@gmail.com>
2023-04-09 23:55:13 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
 
+import Header from "./components/Header";
+import Feed from "./components/Feed";
+import SearchResult from "./components/SearchResult";
+import VideoDetails from "./components/VideoDetails";
+import { AppContext } from './context/ContextApi';
+
+
+
+export default function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <AppContext>
+     <BrowserRouter>
+    <div className='flex flex-col h-full'>
+    <Header/>
+    <Routes>
+    <Route path="/" exect element={<Feed/>}/>
+    <Route path="/searchResult/:searchQuery" element={<SearchResult/>} />
+    <Route path="/video/:id" element={<VideoDetails/>}/>
+    </Routes>
     </div>
+     </BrowserRouter>
+    </AppContext>
   )
 }
-
-export default App
